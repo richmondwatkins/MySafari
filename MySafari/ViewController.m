d8fa2a84731581d40a4c1dcdcd68aef8d62ddb6b
Richmond Watkins <richmondwatkins@mac.com>
2014-10-01 13:50:53 -0500

    add UIWebView and backbutton

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,20 +8,38 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSURL *url = [NSURL URLWithString:self.urlTextField.text];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:urlRequest];
+
+    return YES;
 }
+
+-(void)webViewDidStartLoad:(UIWebView *)webView{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (IBAction)onBackButtonPressed:(id)sender {
+    [self.webView goBack];
+}
+
 
 @end
