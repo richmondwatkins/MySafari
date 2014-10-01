f801e3300c1e394355ea63978c00d2f515707721
Richmond Watkins <richmondwatkins@mac.com>
2014-10-01 13:16:25 -0500

    Initial Commit

---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Richmond on 10/1/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
